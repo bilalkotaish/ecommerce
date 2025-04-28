4b6e1d9f14035d689146f230d1b048380d567f68
Bilalkotaish <you@example.com>
2025-04-28 13:56:05 +0300

    qtybox

--- src/Component/Qtybox/index.jsx
+++ src/Component/Qtybox/index.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-export default function Qtybox(){
-   return(
-  
+import React from "react";
+export default function Qtybox() {
+  return (
     <div className="qtybox">
-    <input type="number" placeholder="qty" className="w-full h-[65px] text-[15px]"/>
+      <input
+        type="number"
+        placeholder="qty"
+        className="w-full h-[45px] text-[15px]  p-2 focus:outline-none !border border-[rgba(0,0,0,0.1)]"
+      />
     </div>
-
-   ) 
+  );
 }
